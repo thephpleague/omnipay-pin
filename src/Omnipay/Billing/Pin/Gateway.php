5e368b56ad15a4f3038bc1aa8a43b2999b0985f1
Adrian Macneil <adrian@adrianmacneil.com>
2013-02-12 18:27:41 +0700

    Omnipay

--- src/Omnipay/Billing/Pin/Gateway.php
+++ src/Omnipay/Billing/Pin/Gateway.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Tala Payments package.
+ * This file is part of the Omnipay package.
  *
  * (c) Adrian Macneil <adrian@adrianmacneil.com>
  *
@@ -9,10 +9,10 @@
  * file that was distributed with this source code.
  */
 
-namespace Tala\Billing\Pin;
+namespace Omnipay\Billing\Pin;
 
-use Tala\AbstractGateway;
-use Tala\Request;
+use Omnipay\AbstractGateway;
+use Omnipay\Request;
 
 /**
  * Pin Gateway
--- tests/Omnipay/Billing/Pin/GatewayTest.php
+++ tests/Omnipay/Billing/Pin/GatewayTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Tala Payments package.
+ * This file is part of the Omnipay package.
  *
  * (c) Adrian Macneil <adrian@adrianmacneil.com>
  *
@@ -9,18 +9,18 @@
  * file that was distributed with this source code.
  */
 
-namespace Tala\Billing\Pin;
+namespace Omnipay\Billing\Pin;
 
 use Mockery as m;
-use Tala\BaseGatewayTest;
-use Tala\CreditCard;
-use Tala\Request;
+use Omnipay\BaseGatewayTest;
+use Omnipay\CreditCard;
+use Omnipay\Request;
 
 class GatewayTest extends BaseGatewayTest
 {
     public function setUp()
     {
-        $this->httpClient = m::mock('\Tala\HttpClient\HttpClientInterface');
+        $this->httpClient = m::mock('\Omnipay\HttpClient\HttpClientInterface');
         $this->httpRequest = m::mock('\Symfony\Component\HttpFoundation\Request');
 
         $this->gateway = new Gateway($this->httpClient, $this->httpRequest);
